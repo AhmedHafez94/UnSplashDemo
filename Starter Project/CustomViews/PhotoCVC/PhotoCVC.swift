89d52f51727f86d8ffab1141550861e54592813b
AhmedHafez94 <gm.ahmed.hafez@gmail.com>
2023-03-30 03:23:57 +0200

    add PhotosCVC

---
+++ Starter Project/CustomViews/PhotoCVC/PhotoCVC.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCVC.swift
+//  Starter Project
+//
+//  Created by Ahmed Hafez on 3/30/23.
+//
+
+import UIKit
+
+class PhotoCVC: UICollectionViewCell {
+
+    @IBOutlet weak var likesLabelCount: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
---
+++ Starter Project/PhotosViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PhotosViewController.swift
+//  Starter Project
+//
+//  Created by Ahmed Hafez on 3/30/23.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet weak var photosCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    func setupCollectionView() {
+        
+      
+        
+    }
+
+
+}
